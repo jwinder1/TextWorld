77068824d373d755bafdea2ae50f89254f3ddc82
JW <jwinder1@umbc.edu>
2019-04-08 17:54:22 -0400

    List packages in setup.py separate from requirements.txt

--- setup.py
+++ setup.py
@@ -38,6 +38,29 @@ class CustomBuildPy(build_py):
         build_py.run(self)
 
 
+required_pkgs = [
+    "numpy>=1.13.1",
+    "tqdm>=4.17.1",
+    "cffi>=1.0.0",
+    "networkx>=2",
+    "pyyaml>=3.12",
+    "urwid>=2.0.1",
+    "more_itertools",
+    "tatsu>=4.3.0",
+    "hashids>=1.2.0",
+    "jericho>=1.1.5",
+    "pybars3>=0.9.3",
+    "flask>=1.0.2",
+    "selenium>=3.12.0",
+    "greenlet==0.4.13",
+    "gevent==1.3.5",
+    "pillow>=5.1.0",
+    "pydot>=1.2.4",
+    "prompt_toolkit<2.1.0,>=2.0.0",
+    "gym>=0.10.11"
+]
+
+
 setup(
     name='textworld',
     version=open(os.path.join("textworld", "version.py")).read().split("=")[-1].strip("' \n"),
@@ -61,7 +84,7 @@ setup(
     description="Microsoft Textworld - A Text-based Learning Environment.",
     cffi_modules=["glk_build.py:ffibuilder"],
     setup_requires=['cffi>=1.0.0'],
-    install_requires=open('requirements.txt').readlines(),
+    install_requires=required_pkgs,
     test_suite='nose.collector',
     tests_require=[
         'nose==1.3.7',
